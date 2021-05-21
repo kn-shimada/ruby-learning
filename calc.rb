b64f14fed9fa62a468d97fe4ed39b4b7227cc2c8
kn-shimada <xianrendaotian012@gmail.com>
2021-05-22 02:06:18 +0900

    クソ雑魚電卓 ver. 2.0

--- calc.rb
+++ calc.rb
@@ -1,66 +1,67 @@
-def add (a, b)
-  return a + b
-end
-
-def subtract (a, b)
-  return a - b
-end
-
-def multiply (a, b)
-  return a * b
-end
-
-def division (a, b)
-  return a / b
-end
-
-def exponentiation (a, b)
-  return a ** b
-end
-
-
-puts "電卓起動\n式を半角で入力してください\n使用できる演算子は、+, -, *, /. **です\n「=」で答えを表示"
-
+puts "電卓起動"
 loop{
-  print"数値:"
-  num_01 = gets.to_i
-  loop{
-    print"演算子:"
-    operator = gets
-    if operator == "=\n"
+  puts "式を＝まで半角で入力してください\n使用できる演算子は、+, -, *, / です\n"
+  formula_get = gets.gsub(" ", "")
+  formula_data = formula_get.split("")
+  formula_data.delete_at(formula_data.size - 1)
+  nums = []
+  operators = []
+  operator_check = false
+  num_check = false
+  ans = 0
+  
+  for formula in formula_data 
+    case formula
+    when "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" then
+      if operator_check or nums.size == 0
+        nums.push(formula)
+        operator_check = false
+        num_check = true
+      end
+
+      if operator_check == false and num_check = false
+        nums[nums.size - 1] = nums[nums.size - 1] + formula
+        num_check = false
+      end
+
+    when "+", "-", "*", "/", "=" then
+      operators.push(formula)
+      operator_check = true
+    else
+      puts "エラー\nプログラム強制終了"
       break
     end
-    
-    print "数値:"
-    num_02 = gets.to_i
-    
-    if operator == "+\n"
-      num_01 = add(num_01, num_02)
+  end
+  nums = nums.map(&:to_i)
+  ans += nums[0]
+
+  for i in 0..operators.size-1 do
+    if operators[i] == "+"
+      ans += nums[i+1]
     end
-    
-    if operator == "-\n"
-      num_01 = subtract(num_01, num_02)
+
+    if operators[i] == "-"
+      ans -= nums[i+1]
     end
-    
-    if operator == "*\n"
-      num_01 = multiply(num_01, num_02)
+
+    if operators[i] == "*"
+      ans *= nums[i+1]
     end
-    
-    if operator == "/\n"
-      num_01 = division(num_01, num_02)
+
+    if operators[i] == "/"
+      ans /= nums[i+1]
     end
-    
-    if operator == "**\n" 
-      num_01 = exponentiation(num_01, num_02)
-    end
-  }
   
-  puts num_01
+    if operators[i] == "="
+      puts "計算終了\n#{formula_get}#{ans}"
+    end
+  end
+
   print "プログラムを終了しますか？ y/n : "
   calc_check = gets 
   if calc_check == "y\n"
     break
   end
-}
+  }
 
 puts "プログラムを終了します。"
